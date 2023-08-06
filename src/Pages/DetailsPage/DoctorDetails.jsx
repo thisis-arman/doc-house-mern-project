37c1d67a01a3f1737b5f118c47a911636e2f0e46
thisis-arman <armanhossen1549@gmail.com>
2023-08-06 19:35:07 +0600

    overview section done

--- src/Pages/DetailsPage/DoctorDetails.jsx
+++ src/Pages/DetailsPage/DoctorDetails.jsx
@@ -1,5 +1,7 @@
 import { useLoaderData } from "react-router-dom";
 import PageCover from "../../Components/PageCover";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 
 const DoctorDetails = () => {
   const data = useLoaderData();
@@ -15,7 +17,7 @@ const DoctorDetails = () => {
             <div className="lg:w-4/5 mx-auto flex flex-wrap shadow-md p-6">
               <img
                 alt="doctor"
-                className="lg:w-1/2 w-full   object-cover object-center rounded"
+                className="lg:w-1/2 w-full   object-cover object-center rounded-none"
                 src={profile.drImage}
               />
               <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
@@ -129,11 +131,10 @@ const DoctorDetails = () => {
                   <p className="text-[#877e7e]">{profile?.Chamber}</p>
                 </div>
                 <div>
-                    <img src="" alt="" />
-                    <img src="" alt="" />
-                    <img src="" alt="" />
-                    <img src="" alt="" />
-
+                  <img src="" alt="" />
+                  <img src="" alt="" />
+                  <img src="" alt="" />
+                  <img src="" alt="" />
                 </div>
 
                 {/* <p className="leading-relaxed">
@@ -144,7 +145,7 @@ const DoctorDetails = () => {
                   listicle pour-over, neutra jean shorts keytar banjo tattooed
                   umami cardigan.
                 </p> */}
-              {/*   <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
+                {/*   <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
                   <div className="flex">
                     <span className="mr-3">Color</span>
                     <button className="border-2 border-gray-300 rounded-full w-6 h-6 focus:outline-none" />
@@ -177,10 +178,14 @@ const DoctorDetails = () => {
                   </div>
                 </div> */}
                 <div>
-                    <button className="btn btn-outline mx-4 py-0 hover:bg-gray-400 hover:text-black">{profile.services[0]}</button>
-                    <button className="btn btn-outline m-4 hover:bg-gray-400 hover:text-black">{profile.services[1]}</button>
+                  <button className="btn btn-outline mr-4 my-4 py-0 hover:bg-gray-400 hover:text-black">
+                    {profile.services[0]}
+                  </button>
+                  <button className="btn btn-outline my-4 hover:bg-gray-400 hover:text-black">
+                    {profile.services[1]}
+                  </button>
                 </div>
-               {/*  <div className="flex">
+                {/*  <div className="flex">
                   <span className="title-font font-medium text-2xl text-gray-900">
                     $58.00
                   </span>
@@ -201,6 +206,105 @@ const DoctorDetails = () => {
                   </button>
                 </div> */}
               </div>
+            </div>
+          </div>
+        </section>
+
+        {/* Overview Section */}
+        <section className="text-gray-600 body-font overflow-hidden ">
+          <div className="container px-5 py-24 mx-auto shadow-xl">
+            <div className="lg:w-4/5 mx-auto flex flex-wrap">
+              <div className=" w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0">
+                <h2 className="text-sm title-font text-gray-500 tracking-widest">
+                  BRAND NAME
+                </h2>
+                <h1 className="text-gray-900 text-3xl title-font font-medium mb-4">
+                  Animated Night Hill Illustrations
+                </h1>
+                <Tabs>
+                <div className="flex mb-4">
+               
+                    <TabList className='w-full bg-base-300 p-4 rounded '>
+                   <div className="flex  ">
+                   <Tab className='border w-1/3 mr-4 hover:bg-gray-300 '>Overview</Tab>
+                      <Tab className='w-1/3 border hover:bg-gray-300 mr-4'>Locations</Tab>
+                      <Tab className='w-1/3 border hover:bg-gray-300 mr-4'>Reviews</Tab>
+                      <Tab className='w-1/3 border hover:bg-gray-300 mr-4'>Business Hours</Tab>
+                   </div>
+                    </TabList>
+                    </div>
+
+                    {/*    <TabPanel>
+      <h2>Any content 1</h2>
+    </TabPanel>
+    <TabPanel>
+      <h2>Any content 2</h2>
+    </TabPanel> */}
+                  
+                
+                <TabPanel>
+                  <div>
+                    <h2 className="text-xl py-2 font-bold">About Me</h2>
+                    <p className="pb-2">{overview?.aboutMe}</p>
+                  </div>
+                  <p className="leading-relaxed mb-4">
+                    Fam locavore kickstarter distillery. Mixtape chillwave
+                    tumeric sriracha taximy chia microdosing tilde DIY. XOXO fam
+                    inxigo juiceramps cornhole raw denim forage brooklyn.
+                    Everyday carry +1 seitan poutine tumeric. Gastropub blue
+                    bottle austin listicle pour-over, neutra jean.
+                  </p>
+                </TabPanel>
+                <TabPanel>
+                  <p className="leading-relaxed mb-4">
+                    Fam locavore kickstarter distillery. Mixtape chillwave
+                    tumeric sriracha taximy chia microdosing tilde DIY. XOXO fam
+                    inxigo juiceramps cornhole raw denim forage brooklyn.
+                    Everyday carry +1 seitan poutine tumeric. Gastropub blue
+                    bottle austin listicle pour-over, neutra jean.
+                  </p>
+                </TabPanel>
+                </Tabs>
+
+                <div className="flex border-t border-gray-200 py-2">
+                  <span className="text-gray-500">Color</span>
+                  <span className="ml-auto text-gray-900">Blue</span>
+                </div>
+                <div className="flex border-t border-gray-200 py-2">
+                  <span className="text-gray-500">Size</span>
+                  <span className="ml-auto text-gray-900">Medium</span>
+                </div>
+                <div className="flex border-t border-b mb-6 border-gray-200 py-2">
+                  <span className="text-gray-500">Quantity</span>
+                  <span className="ml-auto text-gray-900">4</span>
+                </div>
+                <div className="flex">
+                  <span className="title-font font-medium text-2xl text-gray-900">
+                    $58.00
+                  </span>
+                  <button className="flex ml-auto text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded">
+                    Button
+                  </button>
+                  <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
+                    <svg
+                      fill="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      className="w-5 h-5"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z" />
+                    </svg>
+                  </button>
+                </div>
+              </div>
+
+              {/*   <img
+                alt="ecommerce"
+                className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
+                src="https://dummyimage.com/400x400"
+              /> */}
             </div>
           </div>
         </section>
