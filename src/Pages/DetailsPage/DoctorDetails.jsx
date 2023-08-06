25360ff24eee9aa8dab0b6b9a2a1a7eddcd00b8c
thisis-arman <armanhossen1549@gmail.com>
2023-08-06 20:37:03 +0600

    layout designed in doctor profile page

--- src/Pages/DetailsPage/DoctorDetails.jsx
+++ src/Pages/DetailsPage/DoctorDetails.jsx
@@ -11,10 +11,11 @@ const DoctorDetails = () => {
     <section>
       <PageCover pageName="Doctor Profile" />
       {/* Profile section */}
-      <section>
-        <section className="text-gray-600 body-font overflow-hidden">
-          <div className="container px-5 py-24 relative top-[400px] mb-[400px] mx-auto">
-            <div className="lg:w-4/5 mx-auto flex flex-wrap shadow-md p-6">
+      <section className="bg-[#E6E7EE]">
+
+        <section className="text-gray-600 body-font overflow-hidden ">
+          <div className="container rounded-md  py-24 relative top-[320px] mb-[300px] mx-auto shadow-white ">
+            <div className="bg-[#e6e7ee] mx-auto flex flex-wrap shadow-2xl shadow-white drop-shadow-2xl  border border-gray-50 p-6">
               <img
                 alt="doctor"
                 className="lg:w-1/2 w-full   object-cover object-center rounded-none"
@@ -137,46 +138,7 @@ const DoctorDetails = () => {
                   <img src="" alt="" />
                 </div>
 
-                {/* <p className="leading-relaxed">
-                  Fam locavore kickstarter distillery. Mixtape chillwave tumeric
-                  sriracha taximy chia microdosing tilde DIY. XOXO fam indxgo
-                  juiceramps cornhole raw denim forage brooklyn. Everyday carry
-                  +1 seitan poutine tumeric. Gastropub blue bottle austin
-                  listicle pour-over, neutra jean shorts keytar banjo tattooed
-                  umami cardigan.
-                </p> */}
-                {/*   <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
-                  <div className="flex">
-                    <span className="mr-3">Color</span>
-                    <button className="border-2 border-gray-300 rounded-full w-6 h-6 focus:outline-none" />
-                    <button className="border-2 border-gray-300 ml-1 bg-gray-700 rounded-full w-6 h-6 focus:outline-none" />
-                    <button className="border-2 border-gray-300 ml-1 bg-indigo-500 rounded-full w-6 h-6 focus:outline-none" />
-                  </div>
-                  <div className="flex ml-6 items-center">
-                    <span className="mr-3">Size</span>
-                    <div className="relative">
-                      <select className="rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-10">
-                        <option>SM</option>
-                        <option>M</option>
-                        <option>L</option>
-                        <option>XL</option>
-                      </select>
-                      <span className="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
-                        <svg
-                          fill="none"
-                          stroke="currentColor"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth={2}
-                          className="w-4 h-4"
-                          viewBox="0 0 24 24"
-                        >
-                          <path d="M6 9l6 6 6-6" />
-                        </svg>
-                      </span>
-                    </div>
-                  </div>
-                </div> */}
+            
                 <div>
                   <button className="btn btn-outline mr-4 my-4 py-0 hover:bg-gray-400 hover:text-black">
                     {profile.services[0]}
@@ -185,126 +147,118 @@ const DoctorDetails = () => {
                     {profile.services[1]}
                   </button>
                 </div>
-                {/*  <div className="flex">
-                  <span className="title-font font-medium text-2xl text-gray-900">
-                    $58.00
-                  </span>
-                  <button className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
-                    Button
-                  </button>
-                  <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
-                    <svg
-                      fill="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      className="w-5 h-5"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z" />
-                    </svg>
-                  </button>
-                </div> */}
+               
               </div>
             </div>
           </div>
         </section>
 
         {/* Overview Section */}
-        <section className="text-gray-600 body-font overflow-hidden ">
-          <div className="container px-5 pb-24 mx-auto shadow-xl">
+        <section className="text-gray-600 body-font overflow-hidden  ">
+          <div className="container px-5 pb-24 mx-auto   shadow-2xl shadow-white drop-shadow-2xl  border border-gray-50 md:mb-20">
             <div className="lg:w-4/5 mx-auto flex flex-wrap">
               <div className=" w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0">
-               
                 <Tabs>
-                <div className="flex mb-4">
-               
-                    <TabList className='w-full bg-base-300 p-4 rounded '>
-                   <div className="flex  ">
-                   <Tab className='border w-1/3 mr-4 hover:bg-gray-300 '>Overview</Tab>
-                      <Tab className='w-1/3 border hover:bg-gray-300 mr-4'>Locations</Tab>
-                      <Tab className='w-1/3 border hover:bg-gray-300 mr-4'>Reviews</Tab>
-                      <Tab className='w-1/3 border hover:bg-gray-300 mr-4'>Business Hours</Tab>
-                   </div>
+                  <div className="flex mb-4">
+                    <TabList className="w-full bg-base-300 p-4 rounded ">
+                      <div className="flex  ">
+                        <Tab className="border w-1/3 mr-4 hover:bg-gray-300 ">
+                          Overview
+                        </Tab>
+                        <Tab className="w-1/3 border hover:bg-gray-300 mr-4">
+                          Locations
+                        </Tab>
+                        <Tab className="w-1/3 border hover:bg-gray-300 mr-4">
+                          Reviews
+                        </Tab>
+                        <Tab className="w-1/3 border hover:bg-gray-300 mr-4">
+                          Business Hours
+                        </Tab>
+                      </div>
                     </TabList>
-                    </div>
+                  </div>
 
-                    {/*    <TabPanel>
+                  {/*    <TabPanel>
       <h2>Any content 1</h2>
     </TabPanel>
     <TabPanel>
       <h2>Any content 2</h2>
     </TabPanel> */}
-                  
-                
-                <TabPanel>
-                  <div>
-                    {/* About me */}
-                    <h2 className="text-xl py-2 font-bold">About Me</h2>
-                    <p className="pb-2">{overview?.aboutMe}</p>
-                    <div className="grid grid-cols-2  ">
-                      {/* Education */}
-                      <div>
-                        <h2 className="text-xl py-2 font-bold">Education</h2>
-                        <p>
-                          <li className="text-md font-bold ">{education[0]?.institution}</li>
-                          <p className="pl-6">{education[0]?.nameOfDegree}</p>
-                          <p className="pl-6">{education[0]?.Duration}</p>
-                        </p>
-                        <p className="py-4">
-                          <li className="text-md font-bold ">{education[1]?.institution}</li>
-                          <p className="pl-6">{education[1]?.nameOfDegree}</p>
-                          <p className="pl-6">{education[1]?.Duration}</p>
-                        </p>
-                        <p className="py-4">
-                          <li className="text-md font-bold ">{education[2]?.institution}</li>
-                          <p className="pl-6">{education[2]?.nameOfDegree}</p>
-                          <p className="pl-6">{education[2]?.Duration}</p>
-                        </p>
-                      </div>
-                  
 
-                      {/* Awards */}
-                      <div>
-                        <h2 className="text-xl py-2 font-bold ">Awards</h2>
-                        <div className="pl-6">
-                        {
-                          awards.map((data,i)=><p className="py-2" key={i}>
-                            <h3 className="font-bold list-item text-md">{data.awardName}</h3>
-                            <p className="py-2">{data.details}</p>
-                          </p>)
-                        }
+                  <TabPanel>
+                    <div>
+                      {/* About me */}
+                      <h2 className="text-xl py-2 font-bold">About Me</h2>
+                      <p className="pb-2">{overview?.aboutMe}</p>
+                      <div className="grid grid-cols-2  ">
+                        {/* Education */}
+                        <div>
+                          <h2 className="text-xl py-2 font-bold">Education</h2>
+                          <p>
+                            <li className="text-md font-bold ">
+                              {education[0]?.institution}
+                            </li>
+                            <p className="pl-6">{education[0]?.nameOfDegree}</p>
+                            <p className="pl-6">{education[0]?.Duration}</p>
+                          </p>
+                          <p className="py-4">
+                            <li className="text-md font-bold ">
+                              {education[1]?.institution}
+                            </li>
+                            <p className="pl-6">{education[1]?.nameOfDegree}</p>
+                            <p className="pl-6">{education[1]?.Duration}</p>
+                          </p>
+                          <p className="py-4">
+                            <li className="text-md font-bold ">
+                              {education[2]?.institution}
+                            </li>
+                            <p className="pl-6">{education[2]?.nameOfDegree}</p>
+                            <p className="pl-6">{education[2]?.Duration}</p>
+                          </p>
+                        </div>
+
+                        {/* Awards */}
+                        <div>
+                          <h2 className="text-xl py-2 font-bold ">Awards</h2>
+                          <div className="pl-6">
+                            {awards.map((data, i) => (
+                              <p className="py-2" key={i}>
+                                <h3 className="font-bold list-item text-md">
+                                  {data.awardName}
+                                </h3>
+                                <p className="py-2">{data.details}</p>
+                              </p>
+                            ))}
+                          </div>
+                        </div>
+                        {/* Services */}
+
+                        <div>
+                          <h2 className="text-xl py-2 font-bold">Services</h2>
+                          <ol className="pl-6 py-2">
+                            {services.map((data, i) => (
+                              <li className="list-disc " key={i}>
+                                {data}
+                              </li>
+                            ))}
+                          </ol>
                         </div>
                       </div>
-                   {/* Services */}
-
-                      <div>
-                        <h2 className="text-xl py-2 font-bold">Services</h2>
-                        <ol className="pl-6 py-2">
-                          {
-                            services.map((data,i) =><li className="list-disc " key={i}>{data}</li>)
-                          }
-                        </ol>
-                      </div>
-
-
                     </div>
-                  </div>
-                 
-                </TabPanel>
-                <TabPanel>
-                  <p className="leading-relaxed mb-4">
-                    Fam locavore kickstarter distillery. Mixtape chillwave
-                    tumeric sriracha taximy chia microdosing tilde DIY. XOXO fam
-                    inxigo juiceramps cornhole raw denim forage brooklyn.
-                    Everyday carry +1 seitan poutine tumeric. Gastropub blue
-                    bottle austin listicle pour-over, neutra jean.
-                  </p>
-                </TabPanel>
+                  </TabPanel>
+                  <TabPanel>
+                    <p className="leading-relaxed mb-4">
+                      Fam locavore kickstarter distillery. Mixtape chillwave
+                      tumeric sriracha taximy chia microdosing tilde DIY. XOXO
+                      fam inxigo juiceramps cornhole raw denim forage brooklyn.
+                      Everyday carry +1 seitan poutine tumeric. Gastropub blue
+                      bottle austin listicle pour-over, neutra jean.
+                    </p>
+                  </TabPanel>
                 </Tabs>
-                </div>
               </div>
             </div>
+          </div>
         </section>
       </section>
     </section>
