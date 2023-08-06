5092089b8810a71c3d408e0f332709558760bb09
thisis-arman <armanhossen1549@gmail.com>
2023-08-06 20:07:43 +0600

    awards section

--- src/Pages/DetailsPage/DoctorDetails.jsx
+++ src/Pages/DetailsPage/DoctorDetails.jsx
@@ -6,7 +6,7 @@ import "react-tabs/style/react-tabs.css";
 const DoctorDetails = () => {
   const data = useLoaderData();
   console.log(data);
-  const { profile, overview, education, award, services } = data;
+  const { profile, overview, education, awards, services } = data;
   return (
     <section>
       <PageCover pageName="Doctor Profile" />
@@ -212,15 +212,10 @@ const DoctorDetails = () => {
 
         {/* Overview Section */}
         <section className="text-gray-600 body-font overflow-hidden ">
-          <div className="container px-5 py-24 mx-auto shadow-xl">
+          <div className="container px-5 pb-24 mx-auto shadow-xl">
             <div className="lg:w-4/5 mx-auto flex flex-wrap">
               <div className=" w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0">
-                <h2 className="text-sm title-font text-gray-500 tracking-widest">
-                  BRAND NAME
-                </h2>
-                <h1 className="text-gray-900 text-3xl title-font font-medium mb-4">
-                  Animated Night Hill Illustrations
-                </h1>
+               
                 <Tabs>
                 <div className="flex mb-4">
                
@@ -247,7 +242,7 @@ const DoctorDetails = () => {
                     {/* About me */}
                     <h2 className="text-xl py-2 font-bold">About Me</h2>
                     <p className="pb-2">{overview?.aboutMe}</p>
-                    <div>
+                    <div className="grid grid-cols-2  ">
                       {/* Education */}
                       <div>
                         <h2 className="text-xl py-2 font-bold">Education</h2>
@@ -261,8 +256,28 @@ const DoctorDetails = () => {
                           <p className="pl-6">{education[1]?.nameOfDegree}</p>
                           <p className="pl-6">{education[1]?.Duration}</p>
                         </p>
+                        <p className="py-4">
+                          <li className="text-md font-bold ">{education[2]?.institution}</li>
+                          <p className="pl-6">{education[2]?.nameOfDegree}</p>
+                          <p className="pl-6">{education[2]?.Duration}</p>
+                        </p>
                       </div>
-                      {/* Services */}
+                  
+
+                      {/* Awards */}
+                      <div>
+                        <h2 className="text-xl py-2 font-bold ">Awards</h2>
+                        <div className="pl-6">
+                        {
+                          awards.map((data,i)=><p className="py-2" key={i}>
+                            <h3 className="font-bold list-item text-md">{data.awardName}</h3>
+                            <p className="py-2">{data.details}</p>
+                          </p>)
+                        }
+                        </div>
+                      </div>
+                   {/* Services */}
+
                       <div>
                         <h2 className="text-xl py-2 font-bold">Services</h2>
                         <ol className="pl-6 py-2">
@@ -272,17 +287,10 @@ const DoctorDetails = () => {
                         </ol>
                       </div>
 
-                      
+
                     </div>
                   </div>
-                  <p className="leading-relaxed mb-4">
-                    Fam locavore kickstarter distillery. Mixtape chillwave
-                    tumeric sriracha taximy chia microdosing tilde DIY. XOXO fam
-                    inxigo juiceramps cornhole raw denim forage brooklyn.
-                    Everyday carry +1 seitan poutine tumeric. Gastropub blue
-                    bottle austin listicle pour-over, neutra jean.
-                  </p>
-
+                 
                 </TabPanel>
                 <TabPanel>
                   <p className="leading-relaxed mb-4">
@@ -294,48 +302,9 @@ const DoctorDetails = () => {
                   </p>
                 </TabPanel>
                 </Tabs>
-
-                <div className="flex border-t border-gray-200 py-2">
-                  <span className="text-gray-500">Color</span>
-                  <span className="ml-auto text-gray-900">Blue</span>
-                </div>
-                <div className="flex border-t border-gray-200 py-2">
-                  <span className="text-gray-500">Size</span>
-                  <span className="ml-auto text-gray-900">Medium</span>
-                </div>
-                <div className="flex border-t border-b mb-6 border-gray-200 py-2">
-                  <span className="text-gray-500">Quantity</span>
-                  <span className="ml-auto text-gray-900">4</span>
-                </div>
-                <div className="flex">
-                  <span className="title-font font-medium text-2xl text-gray-900">
-                    $58.00
-                  </span>
-                  <button className="flex ml-auto text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded">
-                    Button
-                  </button>
-                  <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
-                    <svg
-                      fill="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      className="w-5 h-5"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z" />
-                    </svg>
-                  </button>
                 </div>
               </div>
-
-              {/*   <img
-                alt="ecommerce"
-                className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
-                src="https://dummyimage.com/400x400"
-              /> */}
             </div>
-          </div>
         </section>
       </section>
     </section>
