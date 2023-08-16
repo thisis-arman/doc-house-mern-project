fc65dfc82ce7ebf5c137352257b55d385491c37c
thisis-arman <armanhossen1549@gmail.com>
2023-08-16 22:05:14 +0600

    some updates on process section

--- src/Pages/BecomeDoctor/Process.jsx
+++ src/Pages/BecomeDoctor/Process.jsx
@@ -4,9 +4,9 @@ const Process = () => {
     return (
         <div>
             <section className="text-gray-600 body-font">
-                <div className="container px-5 py-24 mx-auto">
+                <div className="container px-5 py-12 mx-auto">
                     <div className="text-center mb-20">
-                        <h1 className="sm:text-4xl text-2xl font-bold title-font text-gray-900 mb-4">Raw Denim Heirloom Man Braid</h1>
+                        <h1 className="sm:text-4xl text-2xl font-bold title-font text-gray-900 mb-4">How you'll become successful Doctor</h1>
 
                         <div className="flex mt-6 justify-center">
                             <div className="w-16 h-1 rounded-full bg-green-500 inline-flex" />
@@ -21,7 +21,7 @@ const Process = () => {
                             </div>
                             <div className="flex-grow">
                                 <h2 className="text-gray-900 text-lg title-font font-medium mb-3">Shooting Stars</h2>
-                                <p className="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine, ramps microdosing banh mi pug VHS try-hard.</p>
+                                <p className="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. </p>
                                 <a className="mt-3 text-green-500 inline-flex items-center">Learn More
                                     <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} className="w-4 h-4 ml-2" viewBox="0 0 24 24">
                                         <path d="M5 12h14M12 5l7 7-7 7" />
@@ -39,7 +39,7 @@ const Process = () => {
                             </div>
                             <div className="flex-grow">
                                 <h2 className="text-gray-900 text-lg title-font font-medium mb-3">The Catalyzer</h2>
-                                <p className="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine, ramps microdosing banh mi pug VHS try-hard.</p>
+                                <p className="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. </p>
                                 <a className="mt-3 text-green-500 inline-flex items-center">Learn More
                                     <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} className="w-4 h-4 ml-2" viewBox="0 0 24 24">
                                         <path d="M5 12h14M12 5l7 7-7 7" />
@@ -56,7 +56,7 @@ const Process = () => {
                             </div>
                             <div className="flex-grow">
                                 <h2 className="text-gray-900 text-lg title-font font-medium mb-3">Neptune</h2>
-                                <p className="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine, ramps microdosing banh mi pug VHS try-hard.</p>
+                                <p className="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. </p>
                                 <a className="mt-3 text-green-500 inline-flex items-center">Learn More
                                     <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} className="w-4 h-4 ml-2" viewBox="0 0 24 24">
                                         <path d="M5 12h14M12 5l7 7-7 7" />
@@ -73,7 +73,7 @@ const Process = () => {
                             </div>
                             <div className="flex-grow">
                                 <h2 className="text-gray-900 text-lg title-font font-medium mb-3">Neptune</h2>
-                                <p className="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine, ramps microdosing banh mi pug VHS try-hard.</p>
+                                <p className="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. </p>
                                 <a className="mt-3 text-green-500 inline-flex items-center">Learn More
                                     <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} className="w-4 h-4 ml-2" viewBox="0 0 24 24">
                                         <path d="M5 12h14M12 5l7 7-7 7" />
@@ -82,7 +82,7 @@ const Process = () => {
                             </div>
                         </div>
                     </div>
-                    <button className="flex mx-auto mt-16 text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-600 rounded text-lg">Button</button>
+
                 </div>
             </section>
 
