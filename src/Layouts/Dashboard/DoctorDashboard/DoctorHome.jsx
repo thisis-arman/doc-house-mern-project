ad2c79dfa07a4e82eefac192fc1c4a526762e866
thisis-arman <armanhossen1549@gmail.com>
2023-08-17 22:01:25 +0600

    appointment table

--- src/Layouts/Dashboard/DoctorDashboard/DoctorHome.jsx
+++ src/Layouts/Dashboard/DoctorDashboard/DoctorHome.jsx
@@ -4,35 +4,38 @@ const DoctorHome = () => {
     return (
         <section className='p-4'>
             <h2 className='text-2xl font-bold py-4'>Doctor Home</h2>
-            <div className='flex w-full justify-center gap-4 rounded-md py-8'>
-                <div className='flex p-4 rounded-md items-center border bg-pink-100 gap-2'>
+            <div className='flex  justify-evenly gap-4 rounded-md py-8'>
+                <div className='flex p-4 rounded-md px-8 items-center border bg-pink-200 gap-4'>
                     <SlCalender />
                     <div className=''>
                         <h2 className="text-2xl font-bold">342</h2>
                         <p>Appointments</p>
                     </div>
                 </div>
-                <div className='flex items-center p-4 rounded-md border bg-pink-100 gap-2'>
+                <div className='flex items-center p-4 px-8 rounded-md border bg-green-200 gap-4'>
                     <SlCalender />
                     <div>
                         <h2 className="text-2xl font-bold">342</h2>
                         <p>Appointments</p>
                     </div>
                 </div>
-                <div className='flex items-center p-4 rounded-md border bg-pink-100 gap-2'>
+                <div className='flex items-center p-4 px-8 rounded-md border bg-blue-200 gap-4'>
                     <SlCalender />
                     <div>
                         <h2 className="text-2xl font-bold">342</h2>
                         <p>Appointments</p>
                     </div>
                 </div>
-                <div className='flex items-center border p-4 rounded-md bg-pink-100 gap-2'>
+                <div className='flex items-center border p-4 px-8 rounded-md bg-orange-200 gap-4'>
                     <SlCalender />
                     <div>
                         <h2 className="text-2xl font-bold">342</h2>
                         <p>Appointments</p>
                     </div>
                 </div>
+            </div>
+            <div>
+
             </div>
 
         </section>
