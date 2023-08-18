61b5d04fa71ddc07ca594990f37931f6dbf8a683
thisis-arman <armanhossen1549@gmail.com>
2023-08-18 12:15:25 +0600

    user home data

--- src/Layouts/Dashboard/UserDashboard/UserAppointments.jsx
+++ src/Layouts/Dashboard/UserDashboard/UserAppointments.jsx
@@ -24,7 +24,7 @@ const UserAppointments = () => {
 
 
             <div className="overflow-x-auto ">
-                <table className="min-w-full border divide-y-2 divide-gray-200 bg-white text-sm">
+                <table className="min-w-full border divide-y-2 text-center divide-gray-200 bg-white text-sm">
                     <thead className="ltr:text-left rtl:text-right">
                         <tr>
                             <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
--- src/Layouts/Dashboard/UserDashboard/UserHome.jsx
+++ src/Layouts/Dashboard/UserDashboard/UserHome.jsx
@@ -1,62 +1,67 @@
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../../Provider/AuthProvider";
 
 
 const UserHome = () => {
-    return (
-        <div>
-            <section className="bg-white">
-  <div className="mx-auto max-w-screen-xl px-4 py-12 sm:px-6 md:py-16 lg:px-8">
-    <div className="mx-auto max-w-3xl text-center">
-      <h2 className="text-3xl font-bold text-gray-900 sm:text-4xl">
-        Trusted by eCommerce Businesses
-      </h2>
+  const { user } = useContext(AuthContext)
 
-      <p className="mt-4 text-gray-500 sm:text-xl">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione dolores
-        laborum labore provident impedit esse recusandae facere libero harum
-        sequi.
-      </p>
+  const [appointments, setAppointments] = useState([])
+
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/appointments/${user.email}`)
+      .then(res => res.json())
+      .then(data => setAppointments(data))
+  }, [user])
+
+  return (
+    <div>
+      <section className="bg-white p-8">
+
+        <h2 className="text-4xl font-bold">Welcome ,{user.displayName}</h2>
+        <div className="mx-auto max-w-screen-xl px-4 py-6 sm:px-6 md:py-8 lg:px-8">
+
+
+          <div className="mt-8 sm:mt-12">
+            <dl className="grid grid-cols-1 gap-4 sm:grid-cols-3">
+              <div
+                className="flex flex-col rounded-lg border text-white bg-purple-500 border-gray-100 px-4 py-8 text-center"
+              >
+                <dt className="order-last text-lg font-medium ">
+                  Booked Appointments
+                </dt>
+
+                <dd className="text-4xl font-extrabold  md:text-5xl">
+                  {appointments.length}
+                </dd>
+              </div>
+
+              <div
+                className="flex flex-col rounded-lg border bg-orange-400 text-white border-gray-100 px-4 py-8 text-center"
+              >
+                <dt className="order-last text-lg font-medium ">
+                  Paid Amount
+                </dt>
+
+                <dd className="text-4xl font-extrabold  md:text-5xl">$24</dd>
+              </div>
+
+              <div
+                className="flex flex-col rounded-lg border border-gray-100 bg-green-400 text-white px-4 py-8 text-center"
+              >
+                <dt className="order-last text-lg font-medium ">
+                  Due Amount
+                </dt>
+
+                <dd className="text-4xl font-extrabold  md:text-5xl">$86</dd>
+              </div>
+            </dl>
+          </div>
+        </div>
+      </section>
+
     </div>
-
-    <div className="mt-8 sm:mt-12">
-      <dl className="grid grid-cols-1 gap-4 sm:grid-cols-3">
-        <div
-          className="flex flex-col rounded-lg border border-gray-100 px-4 py-8 text-center"
-        >
-          <dt className="order-last text-lg font-medium text-gray-500">
-            Total Sales
-          </dt>
-
-          <dd className="text-4xl font-extrabold text-blue-600 md:text-5xl">
-            $4.8m
-          </dd>
-        </div>
-
-        <div
-          className="flex flex-col rounded-lg border border-gray-100 px-4 py-8 text-center"
-        >
-          <dt className="order-last text-lg font-medium text-gray-500">
-            Official Addons
-          </dt>
-
-          <dd className="text-4xl font-extrabold text-blue-600 md:text-5xl">24</dd>
-        </div>
-
-        <div
-          className="flex flex-col rounded-lg border border-gray-100 px-4 py-8 text-center"
-        >
-          <dt className="order-last text-lg font-medium text-gray-500">
-            Total Addons
-          </dt>
-
-          <dd className="text-4xl font-extrabold text-blue-600 md:text-5xl">86</dd>
-        </div>
-      </dl>
-    </div>
-  </div>
-</section>
-            
-        </div>
-    );
+  );
 };
 
 export default UserHome;
