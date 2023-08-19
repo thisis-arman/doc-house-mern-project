b2d2e507d0e608ab1920376b95be5b90f8d4fbb2
thisis-arman <armanhossen1549@gmail.com>
2023-08-19 11:25:22 +0600

    contact us page done

--- src/Pages/ContactPage/CTAContact.jsx
+++ src/Pages/ContactPage/CTAContact.jsx
@@ -10,7 +10,7 @@ const CTAContact = () => {
 */}
 
             <section
-                className="overflow-hidden bg-[url(https://images.unsplash.com/photo-1562157873-818bc0726f68?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=654&q=80)] bg-cover bg-top bg-no-repeat"
+                className="overflow-hidden bg-[url(https://img.freepik.com/free-vector/hand-drawn-flat-world-aids-day-background_52683-75184.jpg?w=900&t=st=1692422593~exp=1692423193~hmac=753daa40bbb5a79d393ba2a33c0b9ee64b1a19e2cb553aa9cefab50fd7003c58)] bg-cover bg-bottom bg-no-repeat"
             >
                 <div className="bg-black/25 p-8 md:p-12 lg:px-16 lg:py-24">
                     <div className="text-center sm:text-left ">
