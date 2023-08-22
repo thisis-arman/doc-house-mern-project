54393941270553c6c3db3ff2619f119e5ae3df3e
thisis-arman <armanhossen1549@gmail.com>
2023-08-22 16:27:22 +0600

    table added for req doc

--- src/Layouts/Dashboard/AdminDashboard/DoctorRequest.jsx
+++ src/Layouts/Dashboard/AdminDashboard/DoctorRequest.jsx
@@ -22,8 +22,63 @@ const DoctorRequest = () => {
 
 
     return (
-        <div>
+        <div className="p-10">
             <h1>Doctor request  {doctorReq.length}</h1>
+
+
+            <div>
+                <div className="overflow-x-auto">
+                    <table className="table text-center w-full">
+                        {/* head */}
+                        <thead>
+                            <tr>
+                                <th>
+                                    <label>
+                                        <input type="checkbox" className="checkbox" />
+                                    </label>
+                                </th>
+                                <th>Name</th>
+                                <th>Job</th>
+                                <th>Favorite Color</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {/* row 1 */}
+                            {
+                                doctorReq.map((req, i) => <tr key={i}>
+
+                                    <td>
+                                        <div className="flex items-center space-x-3">
+                                            <div className="avatar">
+                                                <div className="mask mask-squircle w-12 h-12">
+                                                    <img src={req?.image} alt="Avatar Tailwind CSS Component" />
+                                                </div>
+                                            </div>
+
+                                        </div>
+                                    </td>
+
+                                    <td>
+                                        {req?.name}
+
+                                    </td>
+                                    <td>{req?.email}</td>
+                                    <th>
+                                        <button className="btn btn-ghost btn-xs">details</button>
+                                    </th>
+                                </tr>)
+                            }
+
+
+
+                        </tbody>
+
+
+
+                    </table>
+                </div>
+            </div>
         </div>
     );
 };
