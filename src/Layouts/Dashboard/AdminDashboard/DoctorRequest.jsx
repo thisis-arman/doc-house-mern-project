3e55d8461c47153b212a3062ef7f32024680929c
thisis-arman <armanhossen1549@gmail.com>
2023-08-23 20:52:23 +0600

    active  doctors

--- src/Layouts/Dashboard/AdminDashboard/DoctorRequest.jsx
+++ src/Layouts/Dashboard/AdminDashboard/DoctorRequest.jsx
@@ -6,6 +6,7 @@ import Swal from "sweetalert2";
 const DoctorRequest = () => {
 
     const [doctorReq, setDoctorReq] = useState([])
+    const [doctors, setDoctor] = useState([])
 
 
     const handleStatus = (user) => {
@@ -39,7 +40,9 @@ const DoctorRequest = () => {
             .then(data => {
 
                 const pendingDoctor = data.filter(data => data.role === "doctor" && data.status === "pending")
+                const activeDoctors = data.filter(data => data.role === "doctor" && data.status === "active")
                 setDoctorReq(pendingDoctor)
+                setDoctor(activeDoctors)
                 console.log(pendingDoctor)
 
 
@@ -49,9 +52,72 @@ const DoctorRequest = () => {
 
     return (
         <div className="p-10 bg-slate-200">
+
+
+
+
+
+            {/* Active Doctor */}
+            <div className="overflow-x-auto">
+                <h1 className="font-bold text-2xl py-4">Active Doctor  </h1>
+                <table className="table text-center w-full">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>
+                                #
+                            </th>
+                            <th>Profile</th>
+                            <th>Name</th>
+                            <th>Status</th>
+                            <th>Status</th>
+                            <th>Details</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* row 1 */}
+                        {
+                            doctors.map((req, i) => <tr key={i}>
+                                <td>
+                                    {i + 1}
+                                </td>
+
+                                <td>
+                                    <div className="flex items-center space-x-3">
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle w-12 h-12">
+                                                <img src={req?.image} alt="Avatar Tailwind CSS Component" />
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </td>
+
+                                <td>
+                                    {req?.name}
+
+                                </td>
+                                <td>{req?.email}</td>
+                                <th>
+                                    <button className="">{req?.status}</button>
+                                </th>
+                                <th>
+                                    <button onClick={() => handleStatus(req)} className="px-4 bg-green-600 hover:bg-lime-600 rounded text-white">View</button>
+                                </th>
+                            </tr>)
+                        }
+
+
+
+                    </tbody>
+
+
+
+                </table>
+            </div>
+
+            {/* Pending Doctor */}
             <h1 className="font-bold text-2xl py-4">Doctor request  </h1>
-
-
             <div>
                 <div className="overflow-x-auto">
                     <table className="table text-center w-full">
