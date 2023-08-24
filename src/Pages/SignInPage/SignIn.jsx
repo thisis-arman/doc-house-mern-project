1523f82f5b86187511010fa6f8822ba4a9a975f9
thisis-arman <armanhossen1549@gmail.com>
2023-08-24 10:14:20 +0600

    redirect to state

--- src/Pages/SignInPage/SignIn.jsx
+++ src/Pages/SignInPage/SignIn.jsx
@@ -1,34 +1,37 @@
 import { useContext } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../Provider/AuthProvider";
 import toast, { Toaster } from 'react-hot-toast';
 
 const SignIn = () => {
-  const {signIn} =useContext(AuthContext)
+  const { signIn } = useContext(AuthContext)
 
-const navigate = useNavigate()
+  const navigate = useNavigate()
+  const location = useLocation()
+  const from = location.state?.from?.pathname || '/'
 
-  const handleSubmit =(event)=>{
+  const handleSubmit = (event) => {
     event.preventDefault()
-  
-   /*  const formData = new FormData(event.target.value)
-    console.log(formData)  */
+
+    /*  const formData = new FormData(event.target.value)
+     console.log(formData)  */
 
     const form = event.target;
     // const name = form.name.value;
     // const photo = form.photo.value;
     const email = form.email.value;
     const password = form.password.value;
-    console.log(email,password)
-    signIn(email,password)
-    .then((result)=>{
-      const loggedUser = result.user;
-      console.log(loggedUser)
-      navigate('/')
-      toast.success("Signed In Successfully")
-    
-    })
-    .catch(err=>console.log(err))
+    console.log(email, password)
+    signIn(email, password)
+      .then((result) => {
+        const loggedUser = result.user;
+        console.log(loggedUser)
+        navigate('/')
+        toast.success("Signed In Successfully")
+        navigate(from, { replace: true })
+
+      })
+      .catch(err => console.log(err))
 
 
 
@@ -39,45 +42,45 @@ const navigate = useNavigate()
   return (
     <section className="md:flex  md:h-screen ">
       <Toaster
-  position="top-center"
-  reverseOrder={false}
-/>
+        position="top-center"
+        reverseOrder={false}
+      />
       <div className="bg-[#07332F] flex justify-center items-center md:w-1/2">
         <img className="w-1/2 mx-auto" src="../../../doc-house Assets/sign-up-page.png" alt="" />
       </div>
-      
+
       <div className=" md:flex items-center justify-center md:w-1/2 mx-auto md:px-0 px-4 ">
 
-      <form onSubmit={handleSubmit} className="shadow border flex flex-col justify-center items-center  md:w-1/2 p-4 py-10 mx-auto rounded m-8">
-        <h2 className="text-xl font-bold text-center">Sign In to Doc House</h2>
-        <div className="form-control w-full max-w-xs">
-          <label className="label">
-            <span className="label-text">Email</span>
-          </label>
-          <input
-            type="email"
-            name="email"
-            placeholder="Enter your email"
-            className="input input-bordered w-full max-w-xs"
-          />
-        </div>
-        <div className="form-control w-full max-w-xs">
-          <label className="label">
-          
-            <span className="label-text">Password</span>
-          </label>
-          <input
-            type="password"
-            name="password"
-            placeholder="Enter your password"
-            className="input input-bordered w-full max-w-xs"
-          />
-        </div>
-        <button type="submit" className="btn w-full max-w-xs btn-secondary my-4">Sign In</button>
-        <p>New Here? <Link to='/sign-up' className="underline text-blue-500">Sign Up</Link></p>
-      </form>
+        <form onSubmit={handleSubmit} className="shadow border flex flex-col justify-center items-center  md:w-1/2 p-4 py-10 mx-auto rounded m-8">
+          <h2 className="text-xl font-bold text-center">Sign In to Doc House</h2>
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              name="email"
+              placeholder="Enter your email"
+              className="input input-bordered w-full max-w-xs"
+            />
+          </div>
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
 
-        
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Enter your password"
+              className="input input-bordered w-full max-w-xs"
+            />
+          </div>
+          <button type="submit" className="btn w-full max-w-xs btn-secondary my-4">Sign In</button>
+          <p>New Here? <Link to='/sign-up' className="underline text-blue-500">Sign Up</Link></p>
+        </form>
+
+
       </div>
     </section>
   );
