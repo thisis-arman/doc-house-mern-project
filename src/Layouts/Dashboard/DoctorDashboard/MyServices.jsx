9592ed80a53a85c54af70c9f6132c3b5ac659faf
thisis-arman <armanhossen1549@gmail.com>
2023-08-28 22:07:00 +0600

    my services

---
+++ src/Layouts/Dashboard/DoctorDashboard/MyServices.jsx
@@ -0,0 +1,59 @@
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../../Provider/AuthProvider";
+
+
+const MyServices = () => {
+    const { user } = useContext(AuthContext)
+    const [services, setServices] = useState([])
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/api/services${user.email}`)
+            .then(res => res.json())
+            .then(data => setServices(data))
+    }, [user])
+
+
+    return (
+        <section>
+
+            <div>
+                {
+                    services.map((service, i) => <div key={i} href="#" className="relative block overflow-hidden rounded-lg border border-gray-100 p-4 sm:p-6 lg:p-8">
+                        <span className="absolute inset-x-0 bottom-0 h-2 bg-gradient-to-r from-green-300 via-blue-500 to-purple-600" />
+                        <div className="sm:flex sm:justify-between sm:gap-4">
+                            <div>
+                                <h3 className="text-lg font-bold text-gray-900 sm:text-xl">
+                                    {service?.serviceName}
+                                </h3>
+                                <p className="mt-1 text-xs font-medium text-gray-600">By John Doe</p>
+                            </div>
+                            <div className="hidden sm:block sm:shrink-0">
+                                <img alt="Paul Clapton" src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80" className="h-16 w-16 rounded-lg object-cover shadow-sm" />
+                            </div>
+                        </div>
+                        <div className="mt-4">
+                            <p className="max-w-[40ch] text-sm text-gray-500">
+                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. At velit illum
+                                provident a, ipsa maiores deleniti consectetur nobis et eaque.
+                            </p>
+                        </div>
+                        <dl className="mt-6 flex gap-4 sm:gap-6">
+                            <div className="flex flex-col-reverse">
+                                <dt className="text-sm font-medium text-gray-600">Published</dt>
+                                <dd className="text-xs text-gray-500">31st June, 2021</dd>
+                            </div>
+                            <div className="flex flex-col-reverse">
+                                <dt className="text-sm font-medium text-gray-600">Reading time</dt>
+                                <dd className="text-xs text-gray-500">3 minute</dd>
+                            </div>
+                        </dl>
+                    </div>
+                    )
+                }
+            </div>
+
+        </section>
+    );
+};
+
+export default MyServices;
