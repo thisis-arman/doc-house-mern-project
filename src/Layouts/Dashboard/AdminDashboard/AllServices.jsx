0a92bd28c997882de55f041d3292c0564c2c7043
thisis-arman <armanhossen1549@gmail.com>
2023-10-02 10:56:06 +0600

    services action implemented

--- src/Layouts/Dashboard/AdminDashboard/AllServices.jsx
+++ src/Layouts/Dashboard/AdminDashboard/AllServices.jsx
@@ -1,5 +1,8 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import { MdOutlineCancel } from 'react-icons/md'
+import { BsCheck2Square, BsTrash } from 'react-icons/bs'
+import toast, { Toaster } from "react-hot-toast";
 
 
 const AllServices = () => {
@@ -13,19 +16,47 @@ const AllServices = () => {
     console.log({ services })
 
 
+    const handleAcceptService = (id) => {
+        fetch(`http://localhost:5000/api/services-by/${id}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.modifiedCount > 0) {
+                    toast.success("Service Accepted Successfully")
+                }
+            })
+    }
+
+    const handleDelete = (id) => {
+        fetch(`http://localhost:5000/api/services-by/${id}`, {
+            method: 'DELETE',
+            headers: {
+                "Content-Type": "application/json"
+            },
+
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.deletedCount > 0) {
+                    toast.success("Service Deleted Successfully")
+                }
+            })
+    }
+
+
     return (
         <div className="p-6">
+            <Toaster />
             <h1 className="text-3xl font-bold">All Services</h1>
             <section>
-                {/*
-  Heads up! 👋
-
-  This component comes with some `rtl` classes. Please remove them if they are not needed in your project.
-*/}
 
                 <div className="overflow-x-auto py-6">
                     <table className="min-w-full divide-y-2 divide-gray-200 bg-white text-sm p-6">
-                        <thead className="text-left ">
+                        <thead className="text-left bg-neutral-300">
                             <tr >
                                 <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
                                     Service Name
@@ -57,13 +88,33 @@ const AllServices = () => {
                                         <td className="whitespace-nowrap px-4 py-2 text-gray-700">${service?.consultFee}</td>
                                         <td className="whitespace-nowrap px-4 py-2 text-gray-700">{service.status}</td>
                                         <td className="whitespace-nowrap px-4 py-2 text-gray-700">$120,000</td>
-                                        <td className="whitespace-nowrap px-4 py-2">
-                                            <a
-                                                href="#"
-                                                className="inline-block rounded bg-indigo-600 px-4 py-2 text-xs font-medium text-white hover:bg-indigo-700"
-                                            >
-                                                View
-                                            </a>
+                                        <td className="whitespace-nowrap px-4  py-2">
+                                            {service.status == 'active' ?
+                                                <>
+
+                                                    <a onClick={() => handleDelete(service._id)}
+                                                        href="#"
+                                                        className="inline-block rounded bg-base-200 p-2 py-2  font-medium text-black text-xl mx-2"
+                                                    >
+                                                        <BsTrash />
+                                                    </a>
+
+                                                </>
+                                                : <>
+                                                    <a onClick={() => handleAcceptService(service._id)}
+                                                        href="#"
+                                                        className="inline-block rounded bg-base-200 p-2 py-2  font-medium text-black text-xl mx-2"
+                                                    >
+                                                        <BsCheck2Square />
+                                                    </a>
+                                                    <a
+                                                        href="#"
+                                                        className="inline-block rounded bg-base-200 p-2 py-2  font-medium text-black text-xl mx-2"
+                                                    >
+                                                        <MdOutlineCancel />
+                                                    </a>
+                                                </>
+                                            }
                                         </td>
                                     </tr>
                                 )
